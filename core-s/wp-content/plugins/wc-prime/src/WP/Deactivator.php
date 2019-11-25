ec45054b477191f16465301e4e7f9e9c07f61185
qnts028 <qnts028@gmail.com>
2019-11-25 22:24:20 +0700

    init core plugin

---
+++ core-s/wp-content/plugins/wc-prime/src/WP/Deactivator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace WcPrime\WP;
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    WC_PRIME
+ * @subpackage WC_PRIME/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    WC_PRIME
+ * @subpackage WC_PRIME/includes
+ * @author     Your Name <email@example.com>
+ */
+class Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
